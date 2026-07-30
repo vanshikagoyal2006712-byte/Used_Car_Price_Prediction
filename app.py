2027ae397de861d82cf2dcf6489186b492f1a9b1
Vanshika Goyal <vanshikagoyal2006712@gmail.com>
2026-07-30 20:51:21 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,4 +1,34 @@
-import streamlit as st
 
-st.title("Hello Streamlit!")
-st.success("Deployment Successful!")
+import streamlit as st
+import pickle 
+import pandas as pd
+df = pd.read_csv("car_data_updated.csv")
+
+model=pickle.load(open("car_price_model.pkl","rb"))
+st.title("🚗 Used Car Price Prediction")
+st.write("Enter the car details below to estimate its selling price.")
+year=st.number_input("Manufacturing Year",min_value=1990,max_value=2026)
+km_driven=st.number_input("Kilometers Driven")
+fuel=st.selectbox("Fuel Type",["Petrol","Diesel","CNG","LPG"])
+seller=st.selectbox("Seller Type",["Dealer","Individual"])
+transmission=st.selectbox("Transmission",["Manual","Automatic"])
+owner=st.selectbox("Owner",["First","Second","Third","Fourth or above"])
+companies = sorted(df["company"].unique())
+company = st.selectbox(
+    "Select Company",
+    companies
+)
+if st.button("Predict Price"):
+    input_df = pd.DataFrame({
+    "year":[year],
+    "km_driven":[km_driven],
+    "fuel":[fuel],
+    "seller_type":[seller],
+    "transmission":[transmission],
+    "owner":[owner],
+    "company":[company]
+})
+    prediction=model.predict(input_df)
+    st.success(f"Predicted Price: {prediction[0]:,.2f}")
+
+
